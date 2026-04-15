9f8614f16d412906f3113c5750aa4d711d99a471
Lucas <lucaslongo2030@gmail.com>
2026-04-14 22:33:16 -0300

    daas

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from 'astro/config';
-import svelte from '@astrojs/svelte';
-import tailwind from '@astrojs/tailwind';
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  output: 'server',
-  adapter: cloudflare(),
-  integrations: [svelte(), tailwind()],
+  output: 'server', // o 'hybrid'
+  adapter: cloudflare()
 });
