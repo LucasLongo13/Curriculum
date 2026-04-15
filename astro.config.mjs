902e48047c906e0607febf7010898d043fdc7f59
Lucas <lucaslongo2030@gmail.com>
2026-04-14 22:56:26 -0300

    manual add svelte integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
+import svelte from '@astrojs/svelte'; // <--- 1. Importar esto
 
 export default defineConfig({
-  output: 'server', // o 'hybrid'
-  adapter: cloudflare()
+  output: 'server',
+  adapter: cloudflare(),
+  integrations: [svelte()], // <--- 2. Añadir esto aquí
 });
